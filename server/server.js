57b3677ac7666215e368d1e04c88d5e76dc03420
Adam Hinckley <adamhinckley@mac.com>
2019-08-16 12:20:03 -0600

    updates home route message

--- server/server.js
+++ server/server.js
@@ -1,28 +1,26 @@
-const express = require('express');
-const helmet = require('helmet');
-const cors = require('cors');
+const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 
 const server = express();
 
-const authRouter = require('../routers/auth.js');
-const usersRouter = require('../routers/users.js');
-const swRouter = require('../routers/serviceWorkers.js');
-const ticketsRouter = require('../routers/tickets.js');
+const authRouter = require("../routers/auth.js");
+const usersRouter = require("../routers/users.js");
+const swRouter = require("../routers/serviceWorkers.js");
+const ticketsRouter = require("../routers/tickets.js");
 
 server.use(express.json());
 server.use(helmet());
 server.use(cors());
 
-server.use('/auth', authRouter);
-server.use('/users', usersRouter);
-server.use('/serviceWorkers', swRouter);
-server.use('/tickets', ticketsRouter);
+server.use("/auth", authRouter);
+server.use("/users", usersRouter);
+server.use("/serviceWorkers", swRouter);
+server.use("/tickets", ticketsRouter);
 
-
-
-        //home route
-server.get('/', (req,res) => {
-    res.send('go to https://github.com/lambda-build-tipsease/build-tipsease-be for end point documentation')
-})
+//home route
+server.get("/", (req, res) => {
+  res.send("Welcome to the TipsEase API");
+});
 
 module.exports = server;
