7a77254aeca6ee582c7d45f6bbf4220182be47a3
Adam Hinckley <adamhinckley@mac.com>
2019-08-16 09:21:03 -0600

    deletes node_modules and .env from repo

--- routers/auth.js
+++ routers/auth.js
@@ -1,92 +1,105 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const db = require('../server/dbConfig.js');
-const secret = require('../server/secret.js').jwtSecret;
-const bcrypt = require('bcrypt');
-const jwt = require('jsonwebtoken');
+const db = require("../server/dbConfig.js");
+const secret = require("../server/secret.js").jwtSecret;
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
+router.get("/users", (req, res) => {
+  db("users")
+    .then(response => res.status(200).json(response))
+    .catch(err => res.status(500).json(err));
+}); //-----remember to delete this-----
 
-router.get('/users', (req,res) => {
-    db('users')
-        .then(response => res.status(200).json(response))
-        .catch(err => res.status(500).json(err))
-})//-----remember to delete this-----
+router.get("/serviceWorkers", (req, res) => {
+  db("serviceWorkers")
+    .then(response => res.status(200).json(response))
+    .catch(err => res.status(500).json(err));
+}); //-----remember to delete this-----
 
-router.get('/serviceWorkers', (req,res) => {
-    db('serviceWorkers')
-        .then(response => res.status(200).json(response))
-        .catch(err => res.status(500).json(err))
-})//-----remember to delete this-----
+router.post("/users/login", (req, res) => {
+  let { username, password, type } = req.body;
 
-router.post('/users/login', (req,res) => {
-    let { username, password, type } = req.body;
-
-    db(type === 'serviceWorkers' ? 'serviceWorkers' : 'users')
-        .where({ username })
-        .first()
-        .then(user => {
-            if (user && bcrypt.compareSync(password, user.password)) {
-                console.log(user);
-                const token = generateToken(user);
-                res.status(200).json({
-                message: `Welcome, ${user.username}!`,
-                token: token,
-                userInfo: user
-                });
-            } else {
-                res.status(401).json({ message: 'Invalid Credentials' });
-            }
-        })
-        .catch(error => {
-            res.status(404).json({message: "Sorry, cant find that user!"});
+  db(type === "serviceWorkers" ? "serviceWorkers" : "users")
+    .where({ username })
+    .first()
+    .then(user => {
+      if (user && bcrypt.compareSync(password, user.password)) {
+        console.log(user);
+        const token = generateToken(user);
+        res.status(200).json({
+          message: `Welcome, ${user.username}!`,
+          token: token,
+          userInfo: user
         });
-})
+      } else {
+        res.status(401).json({ message: "Invalid Credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(404).json({ message: "Sorry, cant find that user!" });
+    });
+});
 
-router.post('/users/register', (req,res) => { //edited for postgres
-    let user = req.body;
-    const hash = bcrypt.hashSync(user.password, 8);
-    user.password = hash;
+router.post("/users/register", (req, res) => {
+  //edited for postgres
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
 
-    db('users')
-        .insert(user).returning()
-        .then(saved => {
-            res.status(201).json(saved);
-        })
-        .catch(error => {
-            console.log(error);
-            res.status(500).json({message: 'Error registering your account. Either this account already exists, or you didnt complete to form.'});
+  db("users")
+    .insert(user)
+    .returning()
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      console.log(error);
+      res
+        .status(500)
+        .json({
+          message:
+            "Error registering your account. Either this account already exists, or you didnt complete to form."
         });
-})
+    });
+});
 
-router.post('/serviceWorkers/register', (req,res) => { //edited for postgres
-    let user = req.body;
-    const hash = bcrypt.hashSync(user.password, 10);
-    user.password = hash;
-    user.accountBalance = 0;
-    user.rating = 5;
-    user.numOfRatings = 0;
+router.post("/serviceWorkers/register", (req, res) => {
+  //edited for postgres
+  let user = req.body;
+  const hash = bcrypt.hashSync(user.password, 10);
+  user.password = hash;
+  user.accountBalance = 0;
+  user.rating = 5;
+  user.numOfRatings = 0;
 
-    db('serviceWorkers')
-        .insert(user).returning()
-        .then(saved => {
-            res.status(201).json(saved);
-        })
-        .catch(error => {
-            console.log(error);
-            res.status(500).json({message: 'Error registering your account. Either this account already exists, or you didnt complete to form.'});
+  db("serviceWorkers")
+    .insert(user)
+    .returning()
+    .then(saved => {
+      res.status(201).json(saved);
+    })
+    .catch(error => {
+      console.log(error);
+      res
+        .status(500)
+        .json({
+          message:
+            "Error registering your account. Either this account already exists, or you didnt complete to form."
         });
-})
+    });
+});
 
-function generateToken(user){
-    const payload = {
-      subject: user.id,
-      username: user.username
-    };
-    const options = {
-      expiresIn: '1h'
-    };
-    
-    return jwt.sign(payload, secret, options)
-  }
+function generateToken(user) {
+  const payload = {
+    subject: user.id,
+    username: user.username
+  };
+  const options = {
+    expiresIn: "1h"
+  };
+
+  return jwt.sign(payload, secret, options);
+}
 
 module.exports = router;
